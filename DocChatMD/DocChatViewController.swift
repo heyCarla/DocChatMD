bb6c2c28e64849e012e38f0b9e2a961fdd4cc738
Carla Alexander <cg.alexander@gmail.com>
2016-03-29 09:15:40 +1100

    initial commit

---
+++ DocChatMD/DocChatViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DocChatViewController.swift
+//  DocChatMD
+//
+//  Created by Carla Alexander on 2016-03-26.
+//  Copyright © 2016 MagicNoodles. All rights reserved.
+//
+
+import UIKit
+
+final class DocChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
